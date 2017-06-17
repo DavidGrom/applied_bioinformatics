866be1142957c99e109fb59084c77593e1e18ff6
DavidGrom <grommisch.david@gmail.com>
2017-06-17 20:45:19 +0200

    new approach by putting the read function to the loop over file function

--- applied_bioinfo/Assignment2/calculatedist.py
+++ applied_bioinfo/Assignment2/calculatedist.py
@@ -1,41 +1,60 @@
 import argparse
 
 def loop_over_files():
-
-
-    print args.file
-    for in_file in args.file:
-        read_sequence(in_file)
-
-def read_sequence(filename):
-    print 'Here I read in the sequence.'
-    sequences = []
     names = []
+    sequences = []
     sequence = ''
     line_num = -1
-    for line in open(str(filename), 'r'):
-        line = line.strip('\n')
-        if line[0] == '>':
-            name = line[1:]
-            names.append(name)
-            line_num += 1
+    print args.file
+    for in_file in args.file:
+        #read_sequence(in_file)
+        print 'Read in sequence and its name and save to a list.'
+        for line in open(in_file, 'r'):
+            line = line.strip('\n')
+            if line[0] == '>':
+                name = line[1:]
+                names.append(name)
+                line_num += 1
+                #print line_num
+            else:
+                if line_num > -1:
+                    line = line.upper()
+                    sequence += line
+        sequences.append(sequence)
+        sequence = ''
+    #print names
+    #print sequences
+
+
+
+#def read_sequence(filename):
+    #print 'Here I read in the sequence.'
+    #sequences = []
+    #names = []
+    #sequence = ''
+    #line_num = -1
+    #for line in open(str(filename), 'r'):
+        #line = line.strip('\n')
+        #if line[0] == '>':
+            #name = line[1:]
+            #names.append(name)
+            #line_num += 1
             #print line_num
-        else:
-            if line_num > -1:
-                line = line.upper()
-                sequence += line
-    sequences.append(sequence)
+        #else:
+            #if line_num > -1:
+                #line = line.upper()
+                #sequence += line
+    #sequences.append(sequence)
     #return names
     #return sequences
-    print names
-    #print_names(names)
+    #save_names(name)
+    #print sequences
 
-def print_names(names):
-    print names
-    print 'Ja hoffe es klappt.'
-    for i in names:
-        print 'Hallo'
-        print i
+#def save_names(name):
+    #names = []
+    #print 'Ja hoffe es klappt.'
+    #names.append(names)
+    #print names
 
 def calculate_nucfreq():
     print 'Pass in the sequence and calculate the nucleotide frequency.'
